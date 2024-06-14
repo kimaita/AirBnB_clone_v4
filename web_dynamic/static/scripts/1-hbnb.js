758513da49e3cedc247fb92d7c990ba8aaaa78fd
kimaita <johnnieqaym@gmail.com>
2024-06-14 04:48:47 +0300

    added task #1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+$(function () {
+  const checked = {};
+  // any existing checked amenities
+  for (const amen of $('DIV.amenities INPUT:checked')) {
+    checked[amen.dataset.id] = amen.dataset.name;
+  }
+  $('DIV.amenities H4').text(Object.values(checked).join());
+
+  $("DIV.amenities LI INPUT[type='checkbox']").on('change', function () {
+    const id = $(this).data('id');
+    const name = $(this).data('name');
+    if ($(this).is(':checked')) {
+      checked[id] = name;
+    } else {
+      delete checked[id];
+    }
+    $('DIV.amenities H4').text(Object.values(checked).join());
+  });
+});
