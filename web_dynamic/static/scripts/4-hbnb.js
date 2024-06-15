60367484a98cc377199374fe06eaf818dd1f8267
Paul Ogolla <paulotieno2@gmail.com>
2024-06-16 01:01:02 +0300

    amenities

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,5 +1,5 @@
 $(function () {
-  const statusUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  const statusUrl = 'http://0.0.0.0:5001/api/v1/status';
   $.getJSON(statusUrl, function (resp) {
     $('DIV#api_status').toggleClass('available', resp.status === 'OK');
   });
@@ -53,5 +53,41 @@ $(function () {
       delete checked[id];
     }
     $('DIV.amenities H4').text(Object.values(checked).join());
+  });
+
+  $('button').on('click', function () {
+    const data = { amenities: Object.keys(checked) };
+    $.ajax({
+      url: url,
+      type: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify(data),
+      success: function (response) {
+        $('section.places').empty();
+        for (const place of response) {
+          const template = `<article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`;
+          $('section.places').append(template);
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        // Handle error here
+        console.error("Error:", textStatus, errorThrown);
+      }
+    });
   });
 });
